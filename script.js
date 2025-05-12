1e0ccbb86270e5557cb6a30ea79ce625ac08a70f
Rodrigo Silva <rodrigo.codeshub25@gmail.com>
2025-05-12 20:31:16 -0300

    config script2

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //Importa módulos app e browserWindow do Electron
-const { app BrowserWindow } = require('electron'); 
+const { app, BrowserWindow } = require('electron')
+
+//Função para criar a janela principal
+
+function creatWindow () {
+ 
+    //criar nova instancia do BrowserWindow
+    const window = new BrowserWindow({
+        width: 800,
+        height: 600,
+    })
+
+}
+
