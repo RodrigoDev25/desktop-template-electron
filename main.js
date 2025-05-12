088975977ad142fa6bed5e8197c0d092894ad505
Rodrigo Silva <rodrigo.codeshub25@gmail.com>
2025-05-12 20:52:11 -0300

    Html e CSS configurados minimamente

---
+++ main.js
@@ -0,0 +1,22 @@
+//Importa módulos app e browserWindow do Electron
+const { app, BrowserWindow } = require('electron')
+
+//Função para criar a janela principal
+
+function creatWindow () {
+ 
+    //criar nova instancia do BrowserWindow
+    const window = new BrowserWindow({
+        width: 800,
+        height: 600,
+    })
+
+    //carregar o arquivo html na janela
+    window.loadFile('index.html');
+}
+
+//Evento que é acionado quando o Electron está pronto
+app.whenReady().then( () => {
+    creatWindow(); //Chama a função de criar janela
+} )
+
